81cd767af1b43e13f265f368fa924d9564ddb676
chuzhumin98 <chuzhumin98@gmail.com>
2017-12-11 14:56:08 +0800

    add for multi field search

--- TestLucene/src/test/TestIndexSearcher.java
+++ TestLucene/src/test/TestIndexSearcher.java
@@ -21,20 +21,20 @@ public class TestIndexSearcher {
 	private IndexReader reader;
 	private IndexSearcher searcher;
 	private Analyzer analyzer;
-	private QueryParser qp;
+
 	private Map<String, Float> fieldBoosts;
 	
 	/*
 	 * 构造函数，初始化解析器等
 	 */
 	public TestIndexSearcher(String path){
-		analyzer = new IKAnalyzer();
+		analyzer = new IKAnalyzer(); //需要使用同样的分词器
 		try{
 			reader = IndexReader.open(FSDirectory.open(new File(path)));
 			searcher = new IndexSearcher(reader);
 			fieldBoosts = new HashMap<String, Float>();
 			for (int i = 0; i < TestInderWriter.fieldsName.length; i++) {
-				
+				fieldBoosts.put(TestInderWriter.fieldsName[i], TestInderWriter.boostsValue[i]);
 			}
 		}catch(IOException e){
 			e.printStackTrace();
@@ -44,9 +44,9 @@ public class TestIndexSearcher {
 	/*
 	 * 在某个特定域中搜索结果
 	 */
-	public TopDocs searchQueryOneField(String queryString,String field,int maxnum,boolean beAnalyzed){
+	public TopDocs searchQueryOneField(String queryString,String field,int maxnum){
 		try {
-			qp = new QueryParser(Version.LUCENE_35, field, analyzer);			
+			QueryParser qp = new QueryParser(Version.LUCENE_35, field, analyzer);			
 			Query query= qp.parse(queryString);
 			query.setBoost(1.0f);
 			TopDocs results = searcher.search(query, maxnum);
@@ -56,6 +56,24 @@ public class TestIndexSearcher {
 		}
 		return null;
 	}
+	
+	/*
+	 * 在给定的多个域中搜索结果
+	 */
+	public TopDocs searchQueryFields(String queryString, int maxnum){ //已设置fileNames和fieldBoosts
+		try {
+			org.apache.lucene.queryParser.MultiFieldQueryParser mfqp = new org.apache.lucene.queryParser.MultiFieldQueryParser
+					(Version.LUCENE_35, TestInderWriter.fieldsName, analyzer, fieldBoosts);			
+			Query query= mfqp.parse(queryString);
+			query.setBoost(1.0f);
+			TopDocs results = searcher.search(query, maxnum);
+			return results;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 	
 	/*
 	 * 根据docID获取与之对应的doc
@@ -69,16 +87,26 @@ public class TestIndexSearcher {
 		return null;
 	}
 	
+	private static String toDocString(Document doc) {
+		String strings = "";
+		for (int i = 0; i < TestInderWriter.fieldsName.length; i++) {
+			String fieldName = TestInderWriter.fieldsName[i];
+			strings += fieldName + ":" + doc.get(fieldName) + "\n";
+		}
+		return strings;
+	}
+	
 	public static void main(String[] args){
 		TestIndexSearcher search=new TestIndexSearcher("index");	
-		System.out.println("query:工业趋势");
-		TopDocs results=search.searchQueryOneField("2012", "年", 100, true);
+		System.out.println("query:刘洋");
+		//TopDocs results=search.searchQueryOneField("2012", "年", 100);
+		TopDocs results = search.searchQueryFields("刘洋", 100);
 		ScoreDoc[] hits = results.scoreDocs;
 		System.out.println("the result number:"+hits.length);
 		for (int i = 0; i < hits.length; i++) { // output raw format
 			Document doc = search.getDoc(hits[i].doc);
-			System.out.println("top "+ (i+1) + ":"+doc.get("摘要")+
-					" score:"+hits[i].score);
+			System.out.println("top "+ (i+1) + ":\n"+toDocString(doc)+
+					"score:"+hits[i].score);
 		}
 	}
 }
