47e51b565d89b9203c813e7ab14d6ec5624c3aa5
chuzhumin98 <chuzhumin98@gmail.com>
2017-12-25 01:16:44 +0800

    behind for multi fields query

--- SearchEngine/src/server/LuceneServer.java
+++ SearchEngine/src/server/LuceneServer.java
@@ -58,43 +58,92 @@ public class LuceneServer extends HttpServlet{
 		if(pageString != null){
 			page = Integer.parseInt(pageString);
 		}
-		if(queryString == null){
-			System.err.println("the query is null");
-			//request.getRequestDispatcher("/Image.jsp").forward(request, response);
-		} else {
-			JSONObject jsonTotal = new JSONObject();
-			System.out.println("your query:" + queryString);
-			IndexTable table = IndexTable.getInstance();
-			ScoreDoc[] allResult = table.getSearchResult(searchMethod, queryString);
-			if (allResult != null) {
-				ScoreDoc[] hits = showList(allResult, page);
-				if (hits != null) {
-					int docNum = Math.min(PAGE_RESULT, hits.length);
-					for (int k = 0; k < fields.length; k++) {
-						String[] fieldResult = new String [docNum];
-						for (int i = 0; i < hits.length && i < PAGE_RESULT; i++) {
-							Document doc = IndexTable.myEngine[searchMethod].getDoc(hits[i].doc);
-							//输出高亮之后的文本
-							fieldResult[i] = IndexTable.myEngine
-									[searchMethod].hightLightString(queryString, doc.get(fields[k]));
-						}
-						jsonTotal.put(fields[k], fieldResult);
-					}	
-				} else {
-					System.out.println("page null");
+		if (searchMethod != 7) { //不为多域检索的处理方式
+			if(queryString == null){
+				System.err.println("the query is null");
+				//request.getRequestDispatcher("/Image.jsp").forward(request, response);
+			} else {
+				JSONObject jsonTotal = new JSONObject();
+				System.out.println("your query:" + queryString);
+				IndexTable table = IndexTable.getInstance();
+				ScoreDoc[] allResult = table.getSearchResult(searchMethod, queryString);
+				if (allResult != null) {
+					ScoreDoc[] hits = showList(allResult, page);
+					if (hits != null) {
+						int docNum = Math.min(PAGE_RESULT, hits.length);
+						for (int k = 0; k < fields.length; k++) {
+							String[] fieldResult = new String [docNum];
+							for (int i = 0; i < hits.length && i < PAGE_RESULT; i++) {
+								Document doc = IndexTable.myEngine[searchMethod].getDoc(hits[i].doc);
+								//输出高亮之后的文本
+								fieldResult[i] = IndexTable.myEngine
+										[searchMethod].hightLightString(queryString, doc.get(fields[k]));
+							}
+							jsonTotal.put(fields[k], fieldResult);
+						}	
+					} else {
+						System.out.println("page null");
+					}
+				}else{
+					System.out.println("result null");
 				}
-			}else{
-				System.out.println("result null");
+				System.out.println("json:"+jsonTotal.toString());
+				
+				request.setAttribute("resultNum", allResult.length);
+				request.setAttribute("currentQuery",queryString);
+				request.setAttribute("currentPage", page);
+				request.setAttribute("searchMethod", searchMethod);
+				request.setAttribute("results", jsonTotal);
+				request.getRequestDispatcher("/resultShow.jsp").forward(request,
+						response);
 			}
-			System.out.println("json:"+jsonTotal.toString());
-			
-			request.setAttribute("resultNum", allResult.length);
-			request.setAttribute("currentQuery",queryString);
-			request.setAttribute("currentPage", page);
-			request.setAttribute("searchMethod", searchMethod);
-			request.setAttribute("results", jsonTotal);
-			request.getRequestDispatcher("/resultShow.jsp").forward(request,
-					response);
+		} else { //为多域检索的处理方式
+			String[] querys = new String [4];
+			boolean isNull = true;
+			for (int i = 0; i < 4; i++) {
+				querys[i] = request.getParameter("query"+(i+1));
+				if (querys[i] != null) {
+					isNull = false;
+				}
+			}
+			if(isNull){
+				System.err.println("the query is null");
+				//request.getRequestDispatcher("/Image.jsp").forward(request, response);
+			} else {
+				JSONObject jsonTotal = new JSONObject();
+				System.out.println("your query:" + querys[0] + "...");
+				IndexTable table = IndexTable.getInstance();
+				ScoreDoc[] allResult = table.getSearchResult(searchMethod, querys);
+				if (allResult != null) {
+					ScoreDoc[] hits = showList(allResult, page);
+					if (hits != null) {
+						int docNum = Math.min(PAGE_RESULT, hits.length);
+						for (int k = 0; k < fields.length; k++) {
+							String[] fieldResult = new String [docNum];
+							for (int i = 0; i < hits.length && i < PAGE_RESULT; i++) {
+								Document doc = IndexTable.myEngine[searchMethod].getDoc(hits[i].doc);
+								//输出高亮之后的文本
+								//fieldResult[i] = IndexTable.myEngine
+										//[searchMethod].hightLightString(querys, doc.get(fields[k]));
+							}
+							jsonTotal.put(fields[k], fieldResult);
+						}	
+					} else {
+						System.out.println("page null");
+					}
+				}else{
+					System.out.println("result null");
+				}
+				System.out.println("json:"+jsonTotal.toString());
+				
+				request.setAttribute("resultNum", allResult.length);
+				request.setAttribute("currentQuery",queryString);
+				request.setAttribute("currentPage", page);
+				request.setAttribute("searchMethod", searchMethod);
+				request.setAttribute("results", jsonTotal);
+				request.getRequestDispatcher("/multiResultShow.jsp").forward(request,
+						response);
+			}
 		}
 	}
 
